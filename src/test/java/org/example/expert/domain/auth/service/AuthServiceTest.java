13519f92b347f58f6bb44a2ee02c2de67e7ba04f
Sangmin Lee <sart5534@gmail.com>
2024-09-13 16:00:21 +0900

    레벨 3: 중복된 이메일로 회원가입 실패

--- src/test/java/org/example/expert/domain/auth/service/AuthServiceTest.java
+++ src/test/java/org/example/expert/domain/auth/service/AuthServiceTest.java
@@ -5,6 +5,7 @@ import org.example.expert.config.PasswordEncoder;
 import org.example.expert.domain.auth.dto.request.SignupRequest;
 import org.example.expert.domain.auth.dto.response.SigninResponse;
 import org.example.expert.domain.auth.dto.response.SignupResponse;
+import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
 import org.example.expert.domain.user.repository.UserRepository;
@@ -15,8 +16,7 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.spy;
@@ -35,7 +35,7 @@ public class AuthServiceTest {
     private AuthService authService;
 
     @Test
-    public void 회원가입_성공() {
+    void 회원가입_성공() {
         // given
 
         SignupRequest signupRequest = new SignupRequest("test@example.com", "password", "User");
@@ -49,7 +49,7 @@ public class AuthServiceTest {
         given(passwordEncoder.encode(signupRequest.getPassword())).willReturn("encodedPassword");
         given(userRepository.save(any(User.class))).willReturn(spyUser);
 
-        given(jwtUtil.createToken(anyLong(),anyString(),any(UserRole.class))).willReturn("token");
+        given(jwtUtil.createToken(anyLong(), anyString(), any(UserRole.class))).willReturn("token");
         // when
 
         SignupResponse signupResponse = authService.signup(signupRequest);
@@ -59,5 +59,20 @@ public class AuthServiceTest {
         assertEquals("token", signupResponse.getBearerToken());
         verify(userRepository).save(any(User.class));
 
+    }
+
+    @Test
+    void 중복된_이메일로_회원가입_실패() {
+        // given
+
+        SignupRequest signupRequest = new SignupRequest("test@example.com", "password", "User");
+
+        given(userRepository.existsByEmail(anyString())).willReturn(true);
+
+        // when / then
+
+        InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> authService.signup(signupRequest));
+
+        assertEquals("이미 존재하는 이메일입니다.", exception.getMessage());
     }
 }
