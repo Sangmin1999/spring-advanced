375a001dc2fa776d7e321a3fc7cb03dd8dbda5c5
Sangmin Lee <sart5534@gmail.com>
2024-09-16 15:03:23 +0900

    레벨 3 컨트롤러 테스트: 일정 단건 조회 성공

---
+++ src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
@@ -0,0 +1,61 @@
+package org.example.expert.domain.todo.controller;
+
+import org.example.expert.config.JwtUtil;
+import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.todo.dto.response.TodoResponse;
+import org.example.expert.domain.todo.service.TodoService;
+import org.example.expert.domain.user.dto.response.UserResponse;
+import org.example.expert.domain.user.entity.User;
+import org.example.expert.domain.user.enums.UserRole;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TodoController.class)
+public class TodoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TodoService todoService;
+
+    @MockBean
+    private JwtUtil jwtUtil;
+
+    @Test
+    void 일정_단건_조회_성공() throws Exception {
+        // given
+        long todoId = 1L;
+        String title = "title";
+        AuthUser authUser = new AuthUser(1L, "email", UserRole.USER);
+        User user = User.fromAuthUser(authUser);
+        UserResponse userResponse = new UserResponse(user.getId(), user.getEmail());
+        TodoResponse response = new TodoResponse(
+                todoId,
+                title,
+                "contents",
+                "Sunny",
+                userResponse,
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+        // when
+        when(todoService.getTodo(todoId)).thenReturn(response);
+
+        // then
+        mockMvc.perform(get("/todos/{todoId}", todoId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(todoId))
+                .andExpect(jsonPath("$.title").value(title));
+    }
+}
